ca920fd801cca2c42e426de4ea0d6cf292463c62
303182519 <303182519@qq.com>
2015-10-28 20:25:22 +0800

    新增

---
+++ README.md
@@ -0,0 +1,38 @@
+性能上报模块
+=========
+
+> 简介：依赖于ga的上报统计
+
+
+
+
+##主要监控以下核心指标
+
+	//网络时间:
+	performance.timing.responseEnd - performance.timing.navigationStart
+
+	//白屏时间(请求完毕到DOM加载完成):
+	performance.timing.domInteractive - performance.timing.responseEnd
+
+	//可操作时间(解释dom树耗时):
+	performance.timing.domComplete - performance.timing.domInteractive
+
+	//load事件的耗时:
+	performance.timing.loadEventEnd - performance.timing.loadEventStart 
+
+	//总下载时间:
+	performance.timing.loadEventEnd - performance.timing.navigationStart  
+
+
+##performance的时间
+
+
+![](http://img.hb.aicdn.com/f843500aac6a5056fe7a66aee3e7242be3a8a664f5cb-eLPp0X)
+
+
+
+##使用方式：
+	
+	//JS里面直接引用就ok
+	require('performanceReport');
+
---
+++ performance-report.js
@@ -0,0 +1,24 @@
+var performanceReport = function() {
+    var timing = performance.timing;
+    var loadTime = timing.loadEventEnd - timing.navigationStart;//过早获取时,loadEventEnd有时会是0
+    if(loadTime <= 0) {
+    // 未加载完，延迟200ms后继续times方法，直到成功
+        setTimeout(function(){
+            performanceReport();
+        }, 200);
+        return;
+    }
+    var netWorkTime = timing.responseEnd - timing.navigationStart;
+    var initDomTreeTime = timing.domInteractive - timing.responseEnd;
+    var domReadyTime = timing.domComplete - timing.domInteractive; //过早获取时,domComplete有时会是0
+    var loadEventTime = timing.loadEventEnd - timing.loadEventStart;
+    if(window.ga){
+        ga('send', 'timing', {'timingCategory': '核心指标','timingVar': '网络时间','timingValue': netWorkTime});
+        ga('send', 'timing', {'timingCategory': '核心指标','timingVar': '白屏时间(请求完毕到DOM加载完成)','timingValue': initDomTreeTime});
+        ga('send', 'timing', {'timingCategory': '核心指标','timingVar': '可操作时间(解释dom树耗时)','timingValue': domReadyTime});
+        ga('send', 'timing', {'timingCategory': '核心指标','timingVar': 'load事件的耗时','timingValue': loadEventTime});
+        ga('send', 'timing', {'timingCategory': '核心指标','timingVar': '总下载时间','timingValue': loadTime});
+    }
+}
+
+performanceReport();
